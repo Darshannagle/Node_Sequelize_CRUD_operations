24191f24ddfc420ac2aa46065ed5dfd00b9340a9
Darshan Nagle <nagledarshan12@gmail.com>
2024-07-09 17:06:04 +0530

    init

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const e = require('express');
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded())
-// app.get('/', (req, res) => res.send('Hello World!'))
-mongoose.connect("mongodb+srv://nagledarshan12:Darshan108@dan.szrelsh.mongodb.net/?retryWrites=true&w=majority&appName=DAN").then(console.log("connected ")).catch((e) => console.log(e))
+app.get('/', (req, res) => res.send('Hello World!'))
+mongoose.connect("mongodb+srv://nagledarshan12:pass@dan.szrelsh.mongodb.net/?retryWrites=true&w=majority&appName=DAN").then(console.log("connected ")).catch((e) => console.log(e))
 app.get('/api/user', async (req, res) => {
     try {
         const user = await User.find();
